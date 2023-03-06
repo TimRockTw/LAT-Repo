29965a2410183a7ad64564323cd3a051f091d03c
TimRockTw <82629690+TimRockTw@users.noreply.github.com>
2023-03-06 10:47:37 +0800

    week2

--- Week2/FirstData.py
+++ Week2/FirstData.py
@@ -1,7 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv('localman.csv')
+df_110 = pd.read_csv('110.csv')
+df_109 = pd.read_csv('109.csv')
+df_108 = pd.read_csv('108.csv')
+df_107 = pd.read_csv('107.csv')
+df_106 = pd.read_csv('106.csv')
+df_105 = pd.read_csv('105.csv')
+df_104 = pd.read_csv('104.csv')
 
-print(df.to_string())
+df_110
 
-df
+new_df_110 = df_110.drop(["學校代碼","學校名稱"], axis=1)
+new_df_109 = df_109.drop(["學校代碼","學校名稱"], axis=1)
+new_df_108 = df_108.drop(["學校代碼","學校名稱"], axis=1)
+new_df_107 = df_107.drop(["學校代碼","學校名稱"], axis=1)
+new_df_106 = df_106.drop(["學校代碼","學校名稱"], axis=1)
+new_df_105 = df_105.drop(["學校代碼","學校名稱"], axis=1)
+new_df_104 = df_104.drop(["學校代碼","學校名稱"], axis=1)
+
+df_110
