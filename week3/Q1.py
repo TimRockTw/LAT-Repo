66204dea115ed98ef54e24edb477ddbc452ec1be
TimRockTw <82629690+TimRockTw@users.noreply.github.com>
2023-03-15 17:58:23 +0800

    0315

--- week3/Q1.py
+++ week3/Q1.py
@@ -1,4 +1,24 @@
 #0
 import pandas as pd
 
-df_110 = pd.read_csv('Week2/110.csv')
+df_110 = pd.read_csv('data/110.csv')
+df_109 = pd.read_csv('data/109.csv')
+df_108 = pd.read_csv('data/108.csv')
+df_107 = pd.read_csv('data/107.csv')
+df_106 = pd.read_csv('data/106.csv')
+df_105 = pd.read_csv('data/105.csv')
+df_104 = pd.read_csv('data/104.csv')
+
+list_df=[df_110,df_109,df_108,df_107,df_106,df_105,df_104,0]
+
+for i in range(len(list_df)-1):
+    list_df[i]=list_df[i].drop(["學校代碼","學校名稱"], axis=1)
+    list_df[i]['year']=110-i
+
+list_all=pd.concat([list_df[0],list_df[1],list_df[2],list_df[3],list_df[4],list_df[5],list_df[6]],axis=0)
+res=list_all.groupby('year').sum()
+
+sum_of_columns = res.iloc[:].sum(axis=1)
+max_value =sum_of_columns .max()
+max_year = sum_of_columns .loc[sum_of_columns [''] == max_value, 'year'].values[0]
+print(f'{max_year} {max_value}')
