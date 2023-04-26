03363197f28e5181eb099560f3c7c7e274318ae7
TimRockTw <82629690+TimRockTw@users.noreply.github.com>
2023-04-26 17:45:13 +0800

    new

--- Week10/index.js
+++ Week10/index.js
@@ -1,10 +1,27 @@
 'use strict';
-const express=require('expess'),configGet=require('config');
+const express=require('express'),configGet=require('config');
 
-const app =express(); //express 很強
+const {TextAnalyticsClient, AzureKeyCredential}=require("@azure/ai-text-analytics");
+//Azure text sentiment
+const endpoint=configGet.get("ENDPOINT");
+const apikey =configGet.get("TEXT_ANALYICS_API_KEY");
+ const app =express(); 
 
-const port = process.env.PORT||process.env.port||3001;//process.env.PORT||process.env.port是遠端伺服器使用 ||這邊是本地
+const port = process.env.PORT||process.env.port||301;//process.env.PORT||process.env.port是遠端伺服器使用 ||這邊是本地
 
 app.listen(port,()=>{
     console.log(`listening on ${port}`);//`'不一樣`在ESC下面
+    MS_TextSentimentAnalysis()
+    .catch((err) =>{
+        console.error("Error:", err);
+    });
 });
+
+async function MS_TextSentimentAnalysis(){
+    console.log("[MS_TextSentimentAnalysis] in");
+    const analyticsClient  =new TextAnalyticsClient(endpoint,new AzureKeyCredential(apikey));
+    let documents=[];
+    documents.push("沒事");
+    const results=await analyticsClient.analyzeSentiment(documents);
+    console.log("[results]",JSON.stringify(results));
+}
